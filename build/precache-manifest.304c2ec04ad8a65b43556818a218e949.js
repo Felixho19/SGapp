b77dc7e0790a285c61b34bdf388d4e63e38ef696
Felixho19 <32325451+Felixho19@users.noreply.github.com>
2019-08-15 00:53:06 +0800

    add

---
+++ build/precache-manifest.304c2ec04ad8a65b43556818a218e949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73879050c600332b42c25ff83fc25553",
+    "url": "/SGApp/index.html"
+  },
+  {
+    "revision": "3fe87bfddcaa2b7025ee",
+    "url": "/SGApp/static/css/main.6453a928.chunk.css"
+  },
+  {
+    "revision": "65d43f5d7e504024ff9a",
+    "url": "/SGApp/static/js/2.04b184c1.chunk.js"
+  },
+  {
+    "revision": "3fe87bfddcaa2b7025ee",
+    "url": "/SGApp/static/js/main.99e5f4bb.chunk.js"
+  },
+  {
+    "revision": "59e91b407e87a04cdecf",
+    "url": "/SGApp/static/js/runtime~main.93a218d6.js"
+  },
+  {
+    "revision": "5613a2ce125e065eb5e5ccb69cbf3aeb",
+    "url": "/SGApp/static/media/sample.5613a2ce.jpg"
+  }
+]);
